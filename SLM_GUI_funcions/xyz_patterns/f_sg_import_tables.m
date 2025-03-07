8efb1a880ede5876070a7ad6d151774f4e2ef01e
darikoneil <darik.oneil.software@gmail.com>
2025-03-07 06:12:13 -0500

    Added import/export tables

--- SLM_GUI_funcions/xyz_patterns/f_sg_import_tables.m
+++ SLM_GUI_funcions/xyz_patterns/f_sg_import_tables.m
@@ -1,33 +1,47 @@
 function f_sg_import_tables(app)
 %F_SG_IMPORT_TABLES Summary of this function goes here
 %   Detailed explanation goes here
-
     [file, location, ~] = uigetfile('*.parquet', 'Select Tables to Load', app.SLM_ops.save_patterns_dir, 'MultiSelect', 'on');
-    num_tables = length(file);
-    filepaths = cell(1, num_tables);
-    for n = 1:num_tables
-        filepaths{n} = fullfile(location, file{n});
+    if iscell(file)
+        num_tables = length(file);
+        filepaths = cell(1, num_tables);
+        for n = 1:num_tables
+            filepaths{n} = fullfile(location, file{n});
+        end
+    else
+        num_tables = 1;
+        filepaths = cell(1, 1);
+        filepaths{1} = fullfile(location, file);
     end
-    xyz_patterns = app.xyz_patterns;
+    
     for i = 1:num_tables
         coords = parquetread(filepaths{i});
-        parts = split(file{i}, '_');
+        true_file = split(filepaths{i}, '/');
+        true_file = true_file{end};
+        parts = split(true_file, '_');
         name = split(parts{end}, '.');
         name = name{1};
-        slm_region = split(file{i}, strcat(['_', name, '.parquet']));
+        
+        relative_file = split(true_file, '\');
+        slm_region = split(relative_file{end}, '.');
         slm_region = slm_region{1};
-        slm_region = replace(slm_region, '_', ' ');
+        slm_region = split(slm_region, '_');
+        slm_region = strcat([slm_region{1}, ' ', slm_region{2}]);
+        
         pattern = struct();
         pattern.pat_name = name;
         pattern.xyz_pts = coords;
         pattern.SLM_region = slm_region;
-        xyz_patterns = [xyz_patterns, pattern];
+        
+        app.xyz_patterns = [app.xyz_patterns, pattern];
+        app.PatterngroupDropDown.Items = {app.xyz_patterns.pat_name}; 
+        app.CurrentregionDropDown.Value = pattern.SLM_region;
+        app.UIImagePhaseTable.Data = pattern.xyz_pts;
+        app.PatternDropDownCtr.Items = [{'None'}, app.xyz_patterns.pat_name];
+        app.PatternDropDownAI.Items = [{'None'}, app.xyz_patterns.pat_name];
+        app.PatternDropDownCtr.Value = pattern.pat_name;
+        app.PatternDropDownAI.Value = pattern.pat_name;
     end
-    
-    app.xyz_patterns = xyz_patterns;
-    app.PatterngroupDropDown.Items = {app.xyz_patterns.pat_name}; 
-    app.PatternDropDownCtr.Items = [{'None'}, app.xyz_patterns.pat_name];
-    app.PatternDropDownAI.Items = [{'None'}, app.xyz_patterns.pat_name];
 
 end
 
