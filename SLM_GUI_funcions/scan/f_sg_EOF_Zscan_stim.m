7f65ea0c346c8deb2a0e3afc445e043b31b415d9
yuriy <shymkivy@gmail.com>
2025-03-08 15:09:41 -0500

    ai

--- SLM_GUI_funcions/scan/f_sg_EOF_Zscan_stim.m
+++ SLM_GUI_funcions/scan/f_sg_EOF_Zscan_stim.m
@@ -25,7 +25,7 @@ tic;
 
 scan1 = inputSingleScan(session);
 stim_type = round(scan1(2)/5*(num_stim-1)); % 0 is blank, 1-N for N patterns
-f_SLM_update(app.SLM_ops, holo_pointers{1,stim_type}); 
+f_SLM_update(app.SLM_ops, holo_pointers{1,stim_type + 1}); 
 pause(0.01)
 frame_start_times(1) = toc;
 
@@ -36,7 +36,7 @@ while imaging
     scan_frame = scan1(1)+1;
     stim_type = round(scan1(2)/5*(num_stim-1));
     if scan_frame > SLM_frame  % if new frame
-        f_SLM_update(app.SLM_ops, holo_pointers{rem(scan_frame-1,num_planes)+1,stim_type});
+        f_SLM_update(app.SLM_ops, holo_pointers{rem(scan_frame-1,num_planes)+1,stim_type+1});
         frame_start_times(scan_frame) = toc;
         if stim_type~=SLM_stim_type % or change of stim
             SLM_stim_type = stim_type;
@@ -49,7 +49,7 @@ while imaging
             imaging = 0;
         end
     elseif stim_type~=SLM_stim_type % or change of stim
-        f_SLM_update(app.SLM_ops, holo_pointers{rem(scan_frame-1,num_planes)+1,stim_type});
+        f_SLM_update(app.SLM_ops, holo_pointers{rem(scan_frame-1,num_planes)+1,stim_type+1});
         session.outputSingleScan(logical(stim_type)*5);
         SLM_stim_type = stim_type;
         stim_times_types(n_SLM_stim,1) = toc;
